1053512cd2284a26d6ed18f4afa0bca8f384a653
aditi379 <68590280+aditi379@users.noreply.github.com>
2020-08-30 20:16:17 +0530

    Update Library.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -1,10 +1,55 @@
-/*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:50 PM
+/* Created by IntelliJ IDEA.
+ *  Author: Aditi Tyagi(191500840)
+ *  Date: 30/09/2020
+ *  Time: 8:15 PM
  *  File Name : Library.java
- * */
+ */
+
 package definitions;
 
+import java.util.Arrays;
+
 public class Library {
+    private Book[] stock;
+
+    public Library() {
+        this.stock = new Book[10];
+        for (int index = 0; index < stock.length; index++) {
+            stock[index] = new Book("AvailableBook " + (index + 1));
+        }
+    }
+
+    public Library(Book[] stock) {
+        this.stock = stock;
+    }
+
+    public Book[] getStock() {
+        return this.stock;
+    }
+
+    public void setStock(Book[] stock) {
+        this.stock = stock;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Library{" +
+                "stock=" + Arrays.toString(stock) +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Library)) return false;
+        Library library = (Library) o;
+        return Arrays.equals(getStock(), library.getStock());
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(getStock());
+    }
 }
+
